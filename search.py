953496c98d4e25eea5ca84688902836ff842295e
Seth Ketsi <christambassador12@gmail.com>
2024-04-01 21:08:29 +0000

    Solution for Leetcode Problem Merge Two Lists

--- search.py
+++ search.py
@@ -1,2 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
