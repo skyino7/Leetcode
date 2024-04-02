49c9c9bdb82563740d573a780e8e69a6f3ef9edc
Seth Ketsi <christambassador12@gmail.com>
2024-04-02 23:31:16 +0000

    Adding Two Numbers in A LinkedList

---
+++ addTwoNumbers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            sum = carry
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            carry, val = divmod(sum, 10)
+            current.next = ListNode(val)
+            current = current.next
+        return dummy.next
--- isValid.py
+++ isValid.py
@@ -7,8 +7,9 @@ class Solution:
             if char in charDict.values():
                 stack.append(char)
             elif char in charDict.keys():
-                if stack == [] or charDict[char] != stack.pop():
+                if not stack or charDict[char] != stack.pop():
                     return False
             else:
                 return False
-        return stack == []
+
+        return not stack
