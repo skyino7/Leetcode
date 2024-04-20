096c0baa7b72b4b9c33e2aa0a52491104f55f29d
Seth Ketsi <christambassador12@gmail.com>
2024-04-20 21:13:14 +0000

    updated

--- next_permutation.py
+++ next_permutation.py
@@ -6,18 +6,26 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         i = len(nums) - 2
+        # find the first element that is less than its next element
         while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
 
+
+        # 'i' is the index of the first element that is less than its next element
         if i >= 0:
             j = len(nums) - 1
+            # find the first element that is greater than nums[i]
             while nums[j] <= nums[i]:
                 j -= 1
 
+            # swap nums[i] and nums[j]
             nums[i], nums[j] = nums[j], nums[i]
 
+        # reverse the elements after nums[i]
         left, right = i + 1, len(nums) - 1
+
         while left < right:
+            # swap nums[left] and nums[right]
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
@@ -28,4 +36,4 @@ if __name__ == "__main__":
     solve = Solution()
     solve.nextPermutation(arr)
 
-    print(arr)
+    print(*arr)
