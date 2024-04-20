1d533614bea916159bb691f3a671922a33a01656
Seth Ketsi <christambassador12@gmail.com>
2024-04-20 21:07:55 +0000

    Next Permutation

---
+++ next_permutation.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 6, 5, 4]
+    solve = Solution()
+    solve.nextPermutation(arr)
+
+    print(arr)
